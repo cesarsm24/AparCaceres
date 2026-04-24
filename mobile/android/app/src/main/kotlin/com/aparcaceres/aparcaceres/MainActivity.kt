afbf529cf07108ba6fa24db79312327f39f465ef
cesarsm24 <csanchezop@alumnos.unex.es>
2026-04-24 12:20:58 +0200

    chore: scaffold flutter mobile app under mobile/

---
+++ mobile/android/app/src/main/kotlin/com/aparcaceres/aparcaceres/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aparcaceres.aparcaceres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile/lib/shared/constants/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  const AppAssets._();
+
+  static const String _images = 'assets/images';
+
+  static const String logo = '$_images/logo.png';
+}
